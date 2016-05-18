d3af24aa165c6f5b8516151fcc23248a0dd1bc27
dickson <dicksonleonard@gmail.com>
2016-05-18 11:20:05 +0700

    Added title and subtitle on movie cell

--- inmovi/Views/MovieCell.swift
+++ inmovi/Views/MovieCell.swift
@@ -12,13 +12,36 @@ class MovieCell: UICollectionViewCell {
     
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var imageCoverView: UIView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var subtitleLabel: UILabel!
+    
     
     var inspiration: Inspiration? {
         didSet {
             if let inspiration = inspiration {
                 imageView.image = inspiration.backgroundImage
+                titleLabel.text = inspiration.title
+                subtitleLabel.text = inspiration.speaker
             }
         }
+    }
+    
+    override func applyLayoutAttributes(layoutAttributes: UICollectionViewLayoutAttributes) {
+        super.applyLayoutAttributes(layoutAttributes)
+        
+        let standardHeight = MovieLayoutConstants.Cell.standardHeight
+        let featuredHeight = MovieLayoutConstants.Cell.featuredHeight
+        
+        let delta = 1 - ((featuredHeight - CGRectGetHeight(frame)) / (featuredHeight - standardHeight))
+        
+        let minAlpha: CGFloat = 0.3
+        let maxAlpha: CGFloat = 0.75
+        imageCoverView.alpha = maxAlpha - (delta * (maxAlpha - minAlpha))
+        
+        let scale = max(delta, 0.5)
+        titleLabel.transform = CGAffineTransformMakeScale(scale, scale)
+        
+        subtitleLabel.alpha = delta
     }
 
 }
